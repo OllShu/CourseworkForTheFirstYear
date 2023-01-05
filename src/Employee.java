f8141e6a24e56e86fea9cc3c2746fc8f955aa726
dns <shulgaol@mail.ru>
2023-01-05 23:43:40 +0200

    курсовая за 1 курс

---
+++ src/Employee.java
@@ -0,0 +1,47 @@
+import java.util.Objects;
+
+public class Employee {
+    private String fullname;
+    private int department;
+    private int salary;
+    private int counter = 1;
+    public static int id;
+
+
+    public Employee(String fullname, int department, int salary) {
+        this.fullname = fullname;
+        this.department = department;
+        this.salary = salary;
+        this.id = counter;
+        counter++;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return id+" ФИО - " + fullname + " работает в отделе номер " + department + " с зарплатой - " + salary;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Employee[] employeesList = new Employee[10];
+
+        employeesList[0] = new Employee("Иванов Иван Иванович", 1, 70500);
+        employeesList[1] = new Employee("Петров Петр Петрович", 1, 55250);
+        employeesList[2] = new Employee("Сидоров Сидор Сидорович", 1, 52900);
+        employeesList[3] = new Employee("Кузнецов Кузьма Кузьмич", 2, 77200);
+        employeesList[4] = new Employee("Васильев Василий Васильевич", 2, 49900);
+        employeesList[5] = new Employee("Попов Евгений Евгеньевич", 3, 81000);
+        employeesList[6] = new Employee("Смирнов Андрей Андреевич", 3, 33400);
+        employeesList[7] = new Employee("Лебедев Сергей Сергеевич", 4, 99500);
+        employeesList[8] = new Employee("Морозов Дмитрий Дмитриевич", 5, 88100);
+        employeesList[9] = new Employee("Соколов Илья Ильич", 5, 63800);
+
+        printEmployeesList(employeesList);
     }
+    public static void printEmployeesList (Employee[] employeesList) {
+        for (int i = 0; i < employeesList.length; i++) {
+            System.out.println(employeesList[i]);
+        }
+    }
+
 }
